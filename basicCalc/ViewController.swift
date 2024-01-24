cc844958eb7c6ad1eb8f5f65c7a53463daf512c0
Levent KINACI <leventtt@192.168.1.33>
2024-01-25 00:22:42 +0300

    ilk commit

---
+++ basicCalc/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  basicCalc
+//
+//  Created by Levent KINACI on 1/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var ikinciText: UITextField!
+    
+    @IBOutlet weak var ilkText: UITextField!
+    
+    
+    @IBOutlet weak var sonucLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toplamaTiklandi(_ sender: Any) {
+        
+        if let ilkSayi = Int(ilkText.text!){
+            if let ikinciSayi = Int(ikinciText.text!){
+                let sonuc = ilkSayi + ikinciSayi
+                sonucLabel.text = String(sonuc)
+            }
+        }
+        
+        
+    }
+    
+    @IBAction func cikarmaTiklandi(_ sender: Any) {
+        
+        if let ilkSayi = Int(ilkText.text!){
+            if let ikinciSayi = Int(ikinciText.text!){
+                let sonuc = ilkSayi - ikinciSayi
+                sonucLabel.text = String(sonuc)
+            }
+        }
+    }
+    
+    @IBAction func carpmaTiklandi(_ sender: Any) {
+        if let ilkSayi = Int(ilkText.text!){
+            if let ikinciSayi = Int(ikinciText.text!){
+                let sonuc = ilkSayi * ikinciSayi
+                sonucLabel.text = String(sonuc)
+            }
+        }
+    }
+    
+
+    @IBAction func bolmeTiklandi(_ sender: Any) {
+        if let ilkSayi = Int(ilkText.text!){
+            if let ikinciSayi = Int(ikinciText.text!){
+                let sonuc = ilkSayi / ikinciSayi
+                sonucLabel.text = String(sonuc)
+            }
+        }
+    }
+    
+    
+}
+
